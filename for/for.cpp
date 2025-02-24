684a0d995a38b14e832a026d2eec9cb92d312c9c
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-24 17:50:36 +0300

    Добавьте файлы проекта.

---
+++ for/for.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number;
+	cout << "Введите number = ";
+	cin >> number;
+	for (int id = 1; id <= number; id++) // для id от 1 до n с шагом 1 
+		cout << id << "\t";
+}
